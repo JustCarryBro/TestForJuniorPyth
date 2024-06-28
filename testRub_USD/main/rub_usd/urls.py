94470ce6ddeaada8dc080b0d5802ee6aa4166804
JustCarryBro <145977020+JustCarryBro@users.noreply.github.com>
2024-06-28 14:04:02 +0500

    Update urls.py

--- testRub_USD/main/rub_usd/urls.py
+++ testRub_USD/main/rub_usd/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.get_current_usd, name='get_current_usd'),
+    path('get-current-usd/', views.get_current_usd, name='get_current_usd'),
 ]
